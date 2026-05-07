006888f32133f78abc62e3a06b7fa0613c0d0fc1
lilyannelise <lily.canete.goodine@gmail.com>
2026-05-07 18:36:57 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,30 +5,54 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+//        for (int i = 1; i < 100; i++) {
+//
+//            // Find out which numbers divide i.
+//            boolean divisibleBy3 = i % 3 == 0;
+//            boolean divisibleBy5 = i % 5 == 0;
+//
+//            // Print our appropriate result.
+//            if (divisibleBy3 && divisibleBy5) {
+//
+//                System.out.println("Fizz Buzz");
+//
+//            } else if (divisibleBy3) {
+//
+//                System.out.println("Fizz");
+//
+//            } else if (divisibleBy5) {
+//
+//                System.out.println("Buzz");
+//
+//            } else {
+//
+//                System.out.println(i);
+//
+//            }
+//        }
+        int i = 1;
+        while (i < 100){
+            i = doFizzBuzz(i);
         }
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean divisBy3 = i % 3 == 0;
+        boolean divisBy5 = i % 5 == 0;
+
+        if (divisBy3 && divisBy5){
+            System.out.println("Fizz Buzz");
+        }
+        else if (divisBy3){
+            System.out.println("Fizz");
+        }
+        else if (divisBy5){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(i);
+        }
+        i++;
+        return i;
     }
 }
